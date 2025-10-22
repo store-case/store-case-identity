4de041c5a19554f4be9cd44a2c357a339645062d
honee8583 <ekgns0508@gmail.com>
2025-10-22 14:52:20 +0900

    chore: init project

---
+++ src/main/java/com/leedahun/storecaseidentity/StoreCaseIdentityApplication.java
@@ -0,0 +1,13 @@
+package com.leedahun.storecaseidentity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreCaseIdentityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreCaseIdentityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/leedahun/storecaseidentity/StoreCaseIdentityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leedahun.storecaseidentity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreCaseIdentityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
