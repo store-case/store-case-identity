8500baf71e49b426f2a75c4046a25d88e83fee9b
leedahoon <ekgns0508@gmail.com>
2025-10-22 23:49:14 +0900

    feat: UserDetails 구현체 생성

---
+++ src/main/java/com/leedahun/storecaseidentity/domain/auth/dto/LoginUser.java
@@ -0,0 +1,41 @@
+package com.leedahun.storecaseidentity.domain.auth.dto;
+
+import java.util.Collection;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Setter
+@Getter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginUser implements UserDetails {
+
+    private Long id;
+    private String username;
+    private String password;
+    private String role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of((GrantedAuthority) () -> "ROLE_" + role);
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+}
