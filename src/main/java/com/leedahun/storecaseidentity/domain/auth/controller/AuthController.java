bbaa3bcb8a9cfbc57c5bf57846f40c0436ee2a31
honee8583 <ekgns0508@gmail.com>
2025-10-23 14:14:10 +0900

    refactor: test api 제거

--- src/main/java/com/leedahun/storecaseidentity/domain/auth/controller/AuthController.java
+++ src/main/java/com/leedahun/storecaseidentity/domain/auth/controller/AuthController.java
@@ -1,32 +1,21 @@
 package com.leedahun.storecaseidentity.domain.auth.controller;
 
-import com.leedahun.storecaseidentity.common.error.exception.EntityNotFoundException;
 import com.leedahun.storecaseidentity.common.message.SuccessMessage;
 import com.leedahun.storecaseidentity.common.response.HttpResponse;
-import com.leedahun.storecaseidentity.domain.auth.dto.JoinRequestDto;
 import com.leedahun.storecaseidentity.domain.auth.config.JwtProperties;
+import com.leedahun.storecaseidentity.domain.auth.dto.JoinRequestDto;
 import com.leedahun.storecaseidentity.domain.auth.dto.LoginRequestDto;
-import com.leedahun.storecaseidentity.domain.auth.dto.LoginUser;
 import com.leedahun.storecaseidentity.domain.auth.dto.TokenResponseDto;
-import com.leedahun.storecaseidentity.domain.auth.entity.User;
 import com.leedahun.storecaseidentity.domain.auth.exception.RefreshTokenNotExistsException;
-import com.leedahun.storecaseidentity.domain.auth.repository.UserRepository;
 import com.leedahun.storecaseidentity.domain.auth.service.LoginService;
 import com.leedahun.storecaseidentity.domain.auth.util.CookieUtil;
-import com.leedahun.storecaseidentity.domain.auth.util.PrincipalUtil;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -35,8 +24,6 @@ public class AuthController {
 
     private final LoginService loginService;
     private final JwtProperties jwtProperties;
-
-    private final UserRepository userRepository;  // TODO 제거
 
     @PostMapping("/join")
     public ResponseEntity<?> join(@RequestBody JoinRequestDto joinRequestDto) {
@@ -67,15 +54,6 @@ public class AuthController {
         return ResponseEntity.ok()
                 .header(HttpHeaders.SET_COOKIE, newRefreshCookie.toString())
                 .body(new HttpResponse(HttpStatus.OK, SuccessMessage.CREATE_TOKENS.getMessage(), tokens.getAccessToken()));
-    }
-
-    // TODO 제거
-    @GetMapping("/test")
-    public ResponseEntity<?> test(@AuthenticationPrincipal LoginUser loginUser) {
-        Long userId = PrincipalUtil.getUserId(loginUser);
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> new EntityNotFoundException("User", loginUser.getId()));
-        return ResponseEntity.ok().body(user);
     }
 
 }
