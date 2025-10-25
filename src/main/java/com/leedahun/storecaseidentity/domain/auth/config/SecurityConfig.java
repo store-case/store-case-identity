00632708edbdcb1b439206d95cb3125845e95ef3
leedahoon <ekgns0508@gmail.com>
2025-10-25 23:35:42 +0900

    fix: CORS 설정 제거
    - Gateway에서 설정하므로 CORS 설정 제거

--- src/main/java/com/leedahun/storecaseidentity/domain/auth/config/SecurityConfig.java
+++ src/main/java/com/leedahun/storecaseidentity/domain/auth/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import com.leedahun.storecaseidentity.domain.auth.filter.CustomAccessDeniedHandl
 import com.leedahun.storecaseidentity.domain.auth.filter.CustomAuthenticationEntrypoint;
 import com.leedahun.storecaseidentity.domain.auth.filter.JwtAuthorizationFilter;
 import com.leedahun.storecaseidentity.domain.auth.util.JwtUtil;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,9 +15,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -41,15 +37,13 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http, JwtAuthorizationFilter jwtAuthorizationFilter) throws Exception {
         http.headers(headers -> headers.frameOptions(FrameOptionsConfig::disable))
-            .cors(cors -> cors.configurationSource(configurationSource()))
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .csrf(AbstractHttpConfigurer::disable)
             .formLogin(AbstractHttpConfigurer::disable)
             .httpBasic(AbstractHttpConfigurer::disable);
 
         http.authorizeHttpRequests(authorize -> authorize
-                .requestMatchers("/api/auth/join", "/api/auth/login", "/api/auth/join/email", "/api/auth/join/email/verify").permitAll()
-                .requestMatchers("/api/auth/test").permitAll()
+                .requestMatchers("/api/identity/auth/**").permitAll()
         );
 
         http.exceptionHandling(e -> e
@@ -62,18 +56,4 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public CorsConfigurationSource configurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://127.0.0.1:5173"));
-        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "X-Requested-With"));
-        configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
-        configuration.addExposedHeader("Authorization");
-        configuration.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-
-        return source;
-    }
 }
